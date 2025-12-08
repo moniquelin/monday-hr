d1a026ec4eed3f8322f893a2bb5b7f1ecd5f4004
moniquelin <moniquemadelin@gmail.com>
2025-12-08 22:28:16 +0700

    added requireEmployee & requireAdmin middleware

--- internal/api/auth.go
+++ internal/api/auth.go
@@ -2,10 +2,8 @@ package api
 
 import (
 	"errors"
-	"fmt"
 	"net/http"
 
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/moniquelin/monday-hr/internal/data"
 	"github.com/moniquelin/monday-hr/internal/validator"
 )
@@ -67,61 +65,4 @@ func (app *Application) loginHandler(w http.ResponseWriter, r *http.Request) {
 		app.serverErrorResponse(w, r, err)
 	}
 	app.writeJSON(w, 201, envelope{"user": user, "authentication_token": tokenString}, nil)
-}
-
-// authenticate checks whether a user is logged in
-func (app *Application) authenticate(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		// Check Authorization header for JWT token
-		// 1st check: check if empty
-		tokenString := r.Header.Get("Authorization")
-		if tokenString == "" {
-			app.invalidAuthenticationTokenResponse(w, r)
-			return
-		}
-		tokenString = tokenString[len("Bearer "):]
-
-		// 2nd check: verify the token
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			return app.Config.Jwt.Secret, nil
-		})
-		if err != nil || !token.Valid {
-			app.invalidAuthenticationTokenResponse(w, r)
-			return
-		}
-
-		// Get claims
-		claims, ok := token.Claims.(jwt.MapClaims)
-		if !ok {
-			app.invalidAuthenticationTokenResponse(w, r)
-			return
-		}
-
-		fmt.Println(claims)
-
-		// Get user ID from claims
-		userID, ok := claims["sub"].(int64)
-		if !ok {
-			app.invalidAuthenticationTokenResponse(w, r)
-			return
-		}
-
-		// Retrieve the details of the user associated with the authentication token
-		user, err := app.Models.Users.Get(userID)
-		if err != nil {
-			switch {
-			case errors.Is(err, data.ErrRecordNotFound):
-				app.invalidAuthenticationTokenResponse(w, r)
-			default:
-				app.serverErrorResponse(w, r, err)
-			}
-			return
-		}
-
-		// Call contextSetUser() to add the user information to the request context.
-		r = app.contextSetUser(r, user)
-		// Call the next handler in the chain.
-		next.ServeHTTP(w, r)
-	})
 }
---
+++ internal/api/middleware.go
@@ -0,0 +1,91 @@
+package api
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/moniquelin/monday-hr/internal/data"
+)
+
+// authenticate checks whether a user is logged in
+func (app *Application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// Check Authorization header for JWT token
+		// 1st check: check if empty
+		tokenString := r.Header.Get("Authorization")
+		if tokenString == "" {
+			app.invalidAuthenticationTokenResponse(w, r)
+			return
+		}
+		tokenString = tokenString[len("Bearer "):]
+
+		// 2nd check: verify the token
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return app.Config.Jwt.Secret, nil
+		})
+		if err != nil || !token.Valid {
+			app.invalidAuthenticationTokenResponse(w, r)
+			return
+		}
+
+		// Get claims
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			app.invalidAuthenticationTokenResponse(w, r)
+			return
+		}
+
+		fmt.Println(claims)
+
+		// Get user ID from claims
+		userID, ok := claims["sub"].(int64)
+		if !ok {
+			app.invalidAuthenticationTokenResponse(w, r)
+			return
+		}
+
+		// Retrieve the details of the user associated with the authentication token
+		user, err := app.Models.Users.Get(userID)
+		if err != nil {
+			switch {
+			case errors.Is(err, data.ErrRecordNotFound):
+				app.invalidAuthenticationTokenResponse(w, r)
+			default:
+				app.serverErrorResponse(w, r, err)
+			}
+			return
+		}
+
+		// Call contextSetUser() to add the user information to the request context.
+		r = app.contextSetUser(r, user)
+		// Call the next handler in the chain.
+		next.ServeHTTP(w, r)
+	})
+}
+
+// requireAdmin checks whether the given user has the "admin" role
+func (app *Application) requireAdmin(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := app.contextGetUser(r)
+		if user.Role != "admin" {
+			app.errorResponse(w, r, http.StatusUnauthorized, "you must be an admin to access this resource")
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+// requireEmployee checks whether the given user has the "employee" role
+func (app *Application) requireEmployee(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := app.contextGetUser(r)
+		if user.Role != "employee" {
+			app.errorResponse(w, r, http.StatusUnauthorized, "you must be an employee to access this resource")
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
